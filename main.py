fd71747bcb1f8211c98983933fea1b209ac34a7c
EarlJam <earlcoenterprises@gmail.com>
2024-12-23 16:58:46 -0800

    generating asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,6 +13,14 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    all_asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+    Asteroid.containers = (all_asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -18,7 +28,12 @@ def main():
                 if event.type == pygame.QUIT:
                       return
           screen.fill("black")
-          player.draw(screen)
+          for i in drawable:
+                i.draw(screen)
+          for i in updatable:
+                i.update(dt)
+          #player.draw(screen)
+          #player.update(dt)
           pygame.display.flip()
           dt = clock.tick(60) / 1000
           
